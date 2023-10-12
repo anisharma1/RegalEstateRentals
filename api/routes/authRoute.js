a59262833902e0dfbdf6375b5ba99623e337b596
Anisha Sharma <anisharma968@gmail.com>
2023-10-12 16:49:51 +0530

    create and add OAuth component

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { register, signin } from "../controllers/authCtrl.js";
+import { google, register, signin } from "../controllers/authCtrl.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/sign-in", signin);
+router.post("/google", google);
 
 export default router;
