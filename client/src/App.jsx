433ceda33b8536fc8d51f5a0e555a5a6d3bd098e
Anisha Sharma <anisharma968@gmail.com>
2023-10-15 17:50:55 +0530

    add update listing route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,7 @@ export default function App() {
 					<Route path="/profile" element={<Profile />} />
 				</Route>
 				<Route path="/create-listing" element={<CreateListing />} />
+				<Route path="/update-listing/:listingId" element={<UpdateListing />} />
 			</Routes>
 		</BrowserRouter>
 	);
