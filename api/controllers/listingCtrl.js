99a8b822b9aeba59ef3b1679a2e4c28c30736981
Anisha Sharma <anisharma968@gmail.com>
2023-11-01 12:15:07 +0530

    Debug search route

--- api/controllers/listingCtrl.js
+++ api/controllers/listingCtrl.js
@@ -64,19 +64,19 @@ export const getListings = async (req, res, next) => {
 		const limit = parseInt(req.query.limit) || 9;
 		const startIndex = parseInt(req.query.startIndex) || 0;
 		let offer = req.query.offer;
-		if (offer === "undefined" || offer === "false") {
+		if (offer === undefined || offer === "false") {
 			offer = { $in: [false, true] };
 		}
 		let furnished = req.query.furnished;
-		if (furnished === "undefined" || furnished === "false") {
+		if (furnished === undefined || furnished === "false") {
 			furnished = { $in: [false, true] };
 		}
 		let parking = req.query.furnished;
-		if (parking === "undefined" || parking === "false") {
+		if (parking === undefined || parking === "false") {
 			parking = { $in: [false, true] };
 		}
 		let type = req.query.type;
-		if (type === "undefined" || type === "all") {
+		if (type === undefined || type === "all") {
 			parking = { $in: ["sale", "rent"] };
 		}
 		const searchTerm = req.query.searchTerm || "";
