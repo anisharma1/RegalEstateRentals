33ad6e0bb69e32d70ff66043e2cd07c6a4cb8d68
Anisha Sharma <anisharma968@gmail.com>
2023-11-06 15:26:08 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -2,14 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from "path";
 import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 import listingRouter from "./routes/listingRoute.js";
 
 dotenv.config();
-
-const app = express();
 
 const MONGO_URL = process.env.MONGO_URL;
 mongoose
@@ -20,7 +18,9 @@ mongoose
 	.catch((error) => {
 		console.error("Error connecting to MongoDB:", error);
 	});
+const __dirname = path.resolve();
 
+const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -31,6 +31,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
 	const statusCode = err.statusCode || 500;
